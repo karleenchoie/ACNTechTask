a7932c2ab4cfa9ad6a516aba96475b72135bc4d7
Karleen Molinar <krlnchoie@gmail.com>
2023-03-17 15:10:10 +0800

    Initial Commit

---
+++ Shared/ACNTechTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  ACNTechTaskApp.swift
+//  Shared
+//
+//  Created by Karleen Choie Molinar on 3/17/23.
+//
+
+import SwiftUI
+
+@main
+struct ACNTechTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Karleen Choie Molinar on 3/17/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
